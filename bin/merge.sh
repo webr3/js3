6bd9686bfab5d5ab83f98f87cd721e726b92eba7
webr3 <nathan@webr3.org>
2010-11-21 20:58:11 +0000

    propertymap for dc, dc11, cc and void

---
+++ bin/merge.sh
@@ -0,0 +1,3 @@
+cat ../source/curiemap.js ../source/propertymap.js ../source/core.js > ../js3.js
+cat ../js3.js ../source/node.exports.js > ../js3.node.js
+java -jar ../../yui.jar ../js3.js > ../js3.min.js
--- js3.js
+++ js3.js
@@ -158,13 +158,26 @@ var propertymap = (function(map) {
   grddl: ['namespaceTransformation','transformation','transformationProperty','result','profileTransformation'],
   wdrs: ['text','issuedby','matchesregex','notmatchesregex','hasIRI','tag','notknownto','describedby','authenticate',
          'validfrom','validuntil','logo','sha1sum','certified','certifiedby','supportedby','data_error','proc_error','error_code'],
+         
+  dc: ['title','creator','subject','description','publisher','contributor','date','type','format','identifier','source',
+       'language','relation','coverage','rights','audience','alternative','tableOfContents','abstract','created','valid',
+       'available','issued','modified','extent','medium','isVersionOf','hasVersion','isReplacedBy','replaces','isRequiredBy',
+       'requires','isPartOf','hasPart','isReferencedBy','references','isFormatOf','hasFormat','conformsTo','spatial','temporal',
+       'mediator','dateAccepted','dateCopyrighted','dateSubmitted','educationLevel','accessRights','bibliographicCitation',
+       'license','rightsHolder','provenance','instructionalMethod','accrualMethod','accrualPeriodicity','accrualPolicy'],
+  dc11: ['title','creator','subject','description','publisher','contributor','date','type','format','identifier','source',
+         'language','relation','coverage','rights'],
   foaf: ['mbox','mbox_sha1sum','gender','geekcode','dnaChecksum','sha1','based_near','title','nick','jabberID','aimChatID',
          'skypeID','icqChatID','yahooChatID','msnChatID','name','firstName','lastName','givenName','givenname','surname',
          'family_name','familyName','phone','homepage','weblog','openid','tipjar','plan','made','maker','img','depiction',
          'depicts','thumbnail','myersBriggs','workplaceHomepage','workInfoHomepage','schoolHomepage','knows','interest',
          'topic_interest','publications','currentProject','pastProject','fundedBy','logo','topic','primaryTopic','focus',
          'isPrimaryTopicOf','page','theme','account','holdsAccount','accountServiceHomepage','accountName','member',
-         'membershipClass','birthday','age','status']
+         'membershipClass','birthday','age','status'],
+  cc: ['requires','prohibits','jurisdiction','useGuidelines','deprecatedOn','attributionName','license','attributionURL',
+       'morePermissions','permits','legalcode'],
+ 'void': ['statItem','feature','subset','target','sparqlEndpoint','linkPredicate','exampleResource','vocabulary',
+          'subjectsTarget','objectsTarget','dataDump','uriLookupEndpoint','uriRegexPattern'],
 });
 /**
  * JS3 Core
