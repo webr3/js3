0ff0de99266f5220713520c44606542509d3e3b9
webr3 <nathan@webr3.org>
2010-11-22 12:45:56 +0000

    updated merge script

--- bin/merge.sh
+++ bin/merge.sh
@@ -1,3 +1,6 @@
-cat ../source/curiemap.js ../source/propertymap.js ../source/core.js > ../js3.js
-cat ../js3.js ../source/node.exports.js > ../js3.node.js
-java -jar ../../yui.jar ../js3.js > ../js3.min.js
+cd ..
+git submodule init
+git submodule update
+cat source/curiemap.js source/propertymap.js source/core.js > js3.js
+cat js3.js source/node.exports.js > js3.node.js
+java -jar ../yui.jar js3.js > js3.min.js
